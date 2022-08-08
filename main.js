903e748527c8e41668a34f5d4039505252f3adcc
Laila MC <laila.civatti@gmail.com>
2022-08-08 16:59:56 +0200

    fixed bugs on alerts; must apply limit to inputs;

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 //todo: restrict input of wrong characters/values during typing/before submission
+//todo: apply limits to the inputs for height and weight: cant be a 10m person or a 400 kg person...
+//todo: make it responsive (mobile first)
 
-// reference to HTML elements
+// REFERENCES:
+
+// to HTML elements
 const form = document.querySelector("form");
 const weight = document.querySelector("input[name='weight']");
 const height = document.querySelector("input[name='height']");
 const submitBtn = document.querySelector("button");
 
+const wContainer = document.querySelector(".weight");
+const hContainer = document.querySelector(".height");
+
+const wAlert = document.createElement("p");
+const hAlert = document.createElement("p");
+
 let weightValue;
 let heightValue;
+
+// FUNCTIONS:
 
 // to calculate Body Mass Index
 function calcBMI(weight, height) {
@@ -15,7 +27,28 @@ function calcBMI(weight, height) {
     BMI = Math.round(BMI*100)/100;
     console.log(BMI);
     return BMI;
+};
+
+//remove previous alerts:
+function removeAlerts() {
+    if (wAlert.textContent !== '') {
+        weight.classList.remove("alert");
+        wContainer.removeChild(wAlert);
+        wAlert.textContent = '';
+    } else if (hAlert.textContent !== '') {
+        height.classList.remove("alert");
+        hContainer.removeChild(hAlert);
+        hAlert.textContent = '';
+    }
+};
+
+// to remove previous results
+function removeResults() {
+    while (form.lastElementChild.tagName === "P") {
+    form.removeChild(form.lastElementChild);
+    console.log(`Deleted 1 paragraph.`);
 }
+};
 
 // to validate numbers
 function isNum(num) {
@@ -26,24 +59,28 @@ function isNum(num) {
     }
 };
 
+// FUNCTIONALITY:
+
 // begin ready for typing
 weight.focus();
 
+// "Calculate" button event listener:
 submitBtn.addEventListener('click', (e) => {
 
     // avoid sending form
     e.preventDefault();
 
-    weightValue = weight.value;
-    heightValue = height.value;
+    //taking value strings and returning numbers
+    weightValue = Number(weight.value);
+    heightValue = Number(height.value);
+    console.log(weightValue,heightValue);
 
-    // checking if both inputs are there && are numbers
-    if (!isNum(weightValue) && !isNum(heightValue)) {
-        // removing previous results if any
-        while (form.lastElementChild.tagName === "P") {
-            form.removeChild(form.lastElementChild);
-            console.log(`Deleted 1 paragraph.`);
-        };
+    // checking if both inputs are there && are numbers && are not empty strings
+    if (weightValue && heightValue) {
+        
+        // removing alerts and results
+        removeAlerts();
+        removeResults();
         
         // declaring result and paragraphs, then appending main result first
         const result = calcBMI(weightValue, heightValue);
@@ -59,28 +96,33 @@ submitBtn.addEventListener('click', (e) => {
         } else if (18.5 <= result && result < 24.9) {
             paraClassification.textContent = `Keep doing what you're doing, you are in the healthy weight range.`;
         } else if (24.9 <= result && result < 29.9) {
-            paraClassification.textContent = `Maybe try to spend some more calories and avoid those cookies, because you are overweight... But don't stress over it, life is too short. On second thought, go on and eat those cookies!`;
+            paraClassification.textContent = `Maybe try to spend some more calories and avoid those cookies, because you are overweight...
+            But don't stress over it, life is too short. 
+            On second thought, go on and eat those cookies!`;
         } else {
-            paraClassification.textContent = `Okay, it may be time to visit the doctor and make sure you are doing alright, because it seems you are obese. Remember to take care of yourself too.`;
+            paraClassification.textContent = `Okay, it may be time to visit the doctor and make sure you are doing alright, because it seems you are obese.
+            Remember to take care of yourself too.`;
         }
         form.appendChild(paraClassification);
+        weight.value = '';
+        height.value = '';
 
-    } else if (!isNum(weightValue)) {
+    } else if (!weightValue) {
+        removeAlerts();
+        removeResults();
         weight.focus();
         weight.setAttribute("class", "alert");
-        //todo: add error message
-    } else if (!isNum(heightValue)) {
+        wAlert.classList.add("alert");
+        wAlert.textContent = `Please type a number.`;
+        wContainer.appendChild(wAlert);
+
+    } else if (!heightValue) {
+        removeAlerts();
+        removeResults();
         height.focus();
         height.setAttribute("class", "alert");
-        //todo: add error message
+        hAlert.classList.add("alert");
+        hAlert.textContent = `Please type a number.`;
+        hContainer.appendChild(hAlert);
     } 
-});
-
-// emptying inputs
-//todo: empty them only after submission???
-weight.addEventListener('click', () => {
-    weight.value = '';
-});
-height.addEventListener('click', () => {
-    height.value = '';
 });
